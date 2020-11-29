091891bc9aa27abc22613b5a227a2265761ff793
jijoungsoo <jijsx@hotmail.com>
2020-11-29 22:11:44 +0900

    queryDsl 설정

--- demoJpa/src/main/java/com/example/demo/da/service/CmUserRepository.java
+++ demoJpa/src/main/java/com/example/demo/da/service/CmUserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.domain.CmUser;
 
 @Repository
-public interface CmUserRepository extends JpaRepository< CmUser, Long> {
+public interface CmUserRepository extends JpaRepository< CmUser, Long> ,CmUserCustomRepository {
 	
 }
