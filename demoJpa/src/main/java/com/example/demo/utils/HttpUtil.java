79c8b0b93e6da18f50fb763f149dc5dae5f6c2f7
jijoungsoo <jijsx@hotmail.com>
2021-04-14 21:12:35 +0900

    gogo

--- demoJpa/src/main/java/com/example/demo/utils/HttpUtil.java
+++ demoJpa/src/main/java/com/example/demo/utils/HttpUtil.java
@@ -1,11 +1,15 @@
 package com.example.demo.utils;
 
 import java.io.IOException;
+import java.net.http.HttpClient;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.net.ssl.SSLContext;
 
@@ -55,7 +59,7 @@ public class HttpUtil {
 			
 			UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(URL);
 	
-			headers.add("Cookie", "adult_chk=1; user_nickname=dd; member_idx= 11;");
+            headers.add("Cookie","AWSALB=lWtH1NZvQAzA5FijUT741VkqZaAwGkbaLUMcmtkNX0LFljPBl97jN/wTY5uwSwR1/eP+RKWUoBP3ulbw+5PS69rtA7ED9UK/vO8MWqKwL9goo70hxuiQ9mAYRVCH; AWSALBCORS=lWtH1NZvQAzA5FijUT741VkqZaAwGkbaLUMcmtkNX0LFljPBl97jN/wTY5uwSwR1/eP+RKWUoBP3ulbw+5PS69rtA7ED9UK/vO8MWqKwL9goo70hxuiQ9mAYRVCH; vc=234552; _ga_6PD00KC58H=GS1.1.1618325005.1.1.1618325155.0; _ga=GA1.2.691413579.1618325156; _gid=GA1.2.1386655495.1618325156;adult_chk=1; user_nickname=dd; member_idx= 11;");
 	
 			HttpEntity<?> entity = new HttpEntity<>(headers);
             headers.add("x-pjax", "true");            // ajax로 읽어오는 내용인데 이거 그냥 주석하자!! 전체 읽어와도 다를건 크게 없다.
@@ -111,6 +115,7 @@ public class HttpUtil {
 			UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(URL);
 	
 			//headers.add("Cookie", "adult_chk=1; user_nickname=dd; member_idx= 11;");
+            headers.add("Cookie","AWSALB=lWtH1NZvQAzA5FijUT741VkqZaAwGkbaLUMcmtkNX0LFljPBl97jN/wTY5uwSwR1/eP+RKWUoBP3ulbw+5PS69rtA7ED9UK/vO8MWqKwL9goo70hxuiQ9mAYRVCH; AWSALBCORS=lWtH1NZvQAzA5FijUT741VkqZaAwGkbaLUMcmtkNX0LFljPBl97jN/wTY5uwSwR1/eP+RKWUoBP3ulbw+5PS69rtA7ED9UK/vO8MWqKwL9goo70hxuiQ9mAYRVCH; vc=234552; _ga_6PD00KC58H=GS1.1.1618325005.1.1.1618325155.0; _ga=GA1.2.691413579.1618325156; _gid=GA1.2.1386655495.1618325156;adult_chk=1; user_nickname=dd; member_idx= 11;");
 	
 			HttpEntity<?> entity = new HttpEntity<>(headers);
 			ResponseEntity<String> resultMap = restTemplate.exchange(uriBuilder.build().toString(), HttpMethod.GET,entity, String.class);
@@ -139,7 +144,11 @@ public class HttpUtil {
 
     public String httpGet2(String URL){
         String tmp;
-			HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        CloseableHttpClient t = HttpClients.custom()
+        .setMaxConnTotal(3)
+        .setMaxConnPerRoute(3)
+        .build();
+			HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(t);
 			factory.setConnectTimeout(10000); // 타임아웃 설정 5초
 			factory.setReadTimeout(10000);// 타임아웃 설정 5초
 			RestTemplate restTemplate = new RestTemplate(factory);
@@ -148,8 +157,8 @@ public class HttpUtil {
 			headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 			
 			UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(URL);
-	
-			headers.add("Cookie", "adult_chk=1; user_nickname=dd; member_idx= 11;");
+            	
+			//headers.add("Cookie", "adult_chk=1; user_nickname=dd; member_idx= 11;");
             headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36");
             headers.add("upgrade-insecure-requests", "1");
             headers.add("sec-fetch-user", "?1");
@@ -157,13 +166,28 @@ public class HttpUtil {
             headers.add("sec-fetch-mode", "navigate");
             headers.add("sec-fetch-dest", "document");
             headers.add("sec-ch-ua-mobile", "?0");
-            headers.add("sec-ch-ua", "Google Chrome;v=89,Chromium;v=89,;Not A Brand;v=99");
+            headers.add("sec-ch-ua", "\"Google Chrome\";v=\"89\",\"Chromium\";v=\"89\",\";Not A Brand\";v=\"99\"");
             headers.add("pragma", "no-cache");
-            headers.add("cache-control", " no-cache");
+            headers.add("cache-control", "max-age=0");
+            headers.add(":authority", "www.avdbs.com");
+            headers.add(":path", "/menu/dvd.php?dvd_idx=790097");
+            headers.add(":scheme", "https");
+            headers.add("Cookie","AWSALB=lWtH1NZvQAzA5FijUT741VkqZaAwGkbaLUMcmtkNX0LFljPBl97jN/wTY5uwSwR1/eP+RKWUoBP3ulbw+5PS69rtA7ED9UK/vO8MWqKwL9goo70hxuiQ9mAYRVCH; AWSALBCORS=lWtH1NZvQAzA5FijUT741VkqZaAwGkbaLUMcmtkNX0LFljPBl97jN/wTY5uwSwR1/eP+RKWUoBP3ulbw+5PS69rtA7ED9UK/vO8MWqKwL9goo70hxuiQ9mAYRVCH; vc=234552; _ga_6PD00KC58H=GS1.1.1618325005.1.1.1618325155.0; _ga=GA1.2.691413579.1618325156; _gid=GA1.2.1386655495.1618325156");
+            
 	
 			HttpEntity<?> entity = new HttpEntity<>(headers);
 			ResponseEntity<String> resultMap = restTemplate.exchange(uriBuilder.build().toString(), HttpMethod.GET,entity, String.class);
 			tmp = resultMap.getBody();
+            HttpHeaders hds= resultMap.getHeaders();
+
+            Iterator iteratorK = hds.keySet().iterator();
+            while (iteratorK.hasNext()) {
+                Object key = iteratorK.next();
+                List<String> value = hds.get(key);
+                
+                    System.out.println("[key]:" + key + ", [value]:" + value);
+                
+            }
 
 			
  
--- demoJpa/src/test/java/com/example/demo/AC_ACTR_GET.java
+++ demoJpa/src/test/java/com/example/demo/AC_ACTR_GET.java
@@ -5,16 +5,57 @@ import com.example.demo.utils.HttpUtil;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+
 
 public class AC_ACTR_GET {
-	@Test
+
+/*    private WebDriver driver;
+    @BeforeEach
+    public void setUp(){
+        //Chrome	89.0.4389.114 (공식 빌드) (64비트) (cohort: Stable)
+        driver = new FirefoxDriver(); // Driver 생성
+        
+    }
+    @AfterEach
+    public void tearDown(){
+        //driver.quit();  // Driver 종료
+    }
+*/	
+    /*
+    @Test
+    
+    public void test_title(){ //타이틀 확인하는 테스트 코드
+//        System.setProperty("webdriver.chrome.driver", "C:/Users/jijsx/Downloads/chromedriver_win32/chromedriver.exe"); // 다운받은 ChromeDriver 위치를 넣어줍니다.
+        System.setProperty("webdriver.gecko.driver", "C:/Users/jijsx/geckodriver-v0.29.1-win64/geckodriver.exe"); // 다운받은 ChromeDriver 위치를 넣어줍니다.
+        driver.get("https://www.avdbs.com/menu/actor.php?actor_idx=2675"); // URL로 접속하기
+        System.out.println("aaaaaa0");
+        String tmp =driver.getPageSource();
+        String url = driver.getCurrentUrl();
+
+        
+        System.out.println("aaaaaa1");
+        System.out.println(url);
+        System.out.println("aaaaaa2");
+        System.out.println(tmp);
+        
+
+        //Assert.assertThat(driver.getTitle(),is("URL의 Title")); // Title 확인 작업
+    }
+    */
+    @Test
     void download() {
         //String tmp =httpGet("https://www.avdbs.com/");
         HttpUtil h = new HttpUtil();
         //String tmp =h.httpGet("http://www.avdbs.com/menu/actor.php?actor_idx=6826");
-        String tmp =h.httpGet2("http://www.avdbs.com/menu/dvd.php?dvd_idx=781756");
-        
+        String tmp =h.httpGet("https://www.avdbs.com/menu/dvd.php?dvd_idx=790097");
+        //System.out.println(tmp);
         Document doc = Jsoup.parseBodyFragment(tmp);
         String mv_nm =  doc.getElementsByClass("profile_view_top").select(".tomato").text(); //작품번호
         String dvd_img_src  = doc.getElementById("dvd_img_src").attr("src");  
@@ -25,4 +66,9 @@ public class AC_ACTR_GET {
         
         System.out.println(profile_detail.text());
     }
+
+  
+
+     
+
 }
